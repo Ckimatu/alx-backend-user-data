0e206b433b45cc7bb756ac0805293b2830246094
Caroline <carolinekimatu6@gmail.com>
2023-08-08 15:54:03 +0000

    Basic authentication tasks

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -4,12 +4,22 @@
 from flask import jsonify, abort
 from api.v1.views import app_views
 
+
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
     """GET /api/v1/unauthorized
     raise 401 error by using abort
     """
     abort(401, description='Unauthorized')
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """GET /api/v1/forbidden
+    endpoint must raise a 403 error by using abort
+    """
+    abort(403, description='Forbidden')
+
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
